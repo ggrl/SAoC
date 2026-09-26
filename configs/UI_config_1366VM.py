1977d494a55849febebadb7f41a2060acd8d65fa
ggrl <226671839+ggrl@users.noreply.github.com>
2026-09-26 16:54:15 +0200

    Update UI_config_1366VM.py

--- configs/UI_config_1366VM.py
+++ configs/UI_config_1366VM.py
@@ -13,9 +13,9 @@ targetw = targetx2 - targetx1
 targeth = targety2 - targety1
 
 cchatx1 = 11
-cchaty1 = 524
+cchaty1 = 570
 cchatx2 = 340
-cchaty2 = 546
+cchaty2 = 590
 cchatw = cchatx2 - cchatx1
 cchath = cchaty2 - cchaty1
 
@@ -23,31 +23,31 @@ barx1 = 30
 barx2 = 950
 barh = 6
 barw = barx2 - barx1
-healthbarx1 = 30
-healthbary1 = 15
-healthbarx2 = 298
-healthbary2 = 19
+healthbarx1 = 4
+healthbary1 = 23
+healthbarx2 = 255
+healthbary2 = 31
 healthbarw = healthbarx2 - healthbarx1
 healthbarh = healthbary2 - healthbary1
 
-endubarx1 = 30
-endubary1 = 22
-endubarx2 = 312
-endubary2 = 23
+endubarx1 = 4
+endubary1 = 35
+endubarx2 = 128
+endubary2 = 39
 endubarw = endubarx2 - endubarx1
 endubarh = endubary2 - endubary1
 
-manabarx1 = 30
-manabary1 = 25
-manabarx2 = 148
-manabary2 = 31
+manabarx1 = 132
+manabary1 = 35
+manabarx2 = 255
+manabary2 = 39
 manabarw = manabarx2 - manabarx1
 manabarh = manabary2 - manabary1
 
-targetbary1 = 1
-targetbary2 = 13
-targetbarx1 = 30
-targetbarx2 = 299
+targetbarx1 = 4
+targetbary1 = 3
+targetbarx2 = 255
+targetbary2 = 19
 targetbarw = targetbarx2 - targetbarx1
 targetbarh = targetbary2 - targetbary1
 
